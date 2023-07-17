9b676e0a82adf70e8e12efb98ba1b1c3fcd1a29a
songhee <etesong0114@gmail.com>
2023-07-17 16:56:22 +0900

    그래프 js 나누기

---
+++ Flask_v3/static/script.js
@@ -0,0 +1,27 @@
+const ctx = document.getElementById('revenue_Chart');
+    
+
+function Pie_Chart() {
+    new Chart(ctx, {
+        type: 'pie',
+        data: {
+        labels: labels ,
+        datasets: [
+            {
+            label: '고객 수',
+            data: data ,
+        backgroundColor: [
+            'rgb(29, 91, 121)',
+            'rgb(70, 139, 151)',
+            'rgb(239, 98, 98)',
+            'rgb(243, 170, 96)',
+            'rgb(74, 85, 162)',
+            'rgb(255, 102, 102)'
+            ],
+            hoverOffset: 50,
+        }]
+        }
+    });
+}
+Pie_Chart()
+    
