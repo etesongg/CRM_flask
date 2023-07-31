cb13a409b54a78a10db08ab2453c04aab910b3a8
songhee <etesong0114@gmail.com>
2023-07-31 17:00:28 +0900

    user페이지 데이터 보여주기까지 완성

--- Flask_v4/views/user.py
+++ Flask_v4/views/user.py
@@ -21,12 +21,14 @@ def index():
     # db 읽기 & 검색 결과에 따른 데이터 보여주기
     
     users_seach = User.query.filter(User.name.like('%'+search_name+'%'), User.gender.like(search_gender+'%')).all()
-
+    # print(users_seach)
     # query = "SELECT * FROM user WHERE name like ? AND gender like ?"
     # headers, datas = dbdata.read_data_db(query,('%'+search_name+'%', search_gender+'%', ))
         
     # 페이지 계산
     total_pages, page, page_data = calc_pages(users_seach, per_page, page)
+
+    headers = ['Id', 'Name', 'Gender', 'Age', 'Birthdate', 'Address']
 
     # 그래프
     query = """
@@ -46,7 +48,9 @@ def index():
     count_data = int(count_data[0]['count(*)'])
     print(count_data)
     
-    return render_template('users.html', headers=users_seach, page_data=page_data, total_pages=total_pages, search_name=search_name, search_gender=search_gender, current_page=page, rows=rows, labels=labels, values=values, count_data=count_data, users_seach=users_seach)
+    print(page_data)
+
+    return render_template('users.html', headers=headers, page_data=page_data, total_pages=total_pages, search_name=search_name, search_gender=search_gender, current_page=page, rows=rows, labels=labels, values=values, count_data=count_data)
 
 @user_bp.route('/user_detail/<id>')
 def user_detail(id):
